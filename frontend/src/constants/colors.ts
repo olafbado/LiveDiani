e8d45a156b55a6db9ab7374452dfc8bf99ee7e82
olafbado <niebiel1@gmail.com>
2025-04-01 15:17:23 +0300

    Add color constants

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,25 @@
+const Colors = {
+  // Tła i karty
+  background: '#F5F5DC',        // piaskowy
+  cardBackground: '#FFFFFF',
+
+  // Tekst i pomocnicze
+  text: '#5D4037',              // safari brąz
+  muted: '#8D6E63',
+
+  // Akcenty
+  primary: '#D84315',           // terracotta
+  secondary: '#A1887F',         // gliniasty brąz
+  danger: '#FF3B30',
+  success: '#8BC34A',
+  white: '#FFFFFF',
+
+  // Kategorie
+  category: {
+    party: '#A1887F',           // neutralny gliniany
+    yoga: '#C5E1A5',            // naturalna zieleń
+    networking: '#FF7043',      // pomarańczowe safari
+  },
+};
+
+export default Colors;
