395ffcfb560037a64922dceb25f228e936d9d2e7
olafbado <niebiel1@gmail.com>
2025-05-04 13:43:51 +0100

    Refactor AppDbContext

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -1,11 +1,12 @@
-using Microsoft.EntityFrameworkCore;
 using backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
 
     public DbSet<Event> Events => Set<Event>();
     public DbSet<EventCategory> EventCategories => Set<EventCategory>();
@@ -19,29 +20,31 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // EventTag (many-to-many)
-        modelBuilder.Entity<EventTag>()
-            .HasKey(et => new { et.EventId, et.TagId });
+        modelBuilder.Entity<EventTag>().HasKey(et => new { et.EventId, et.TagId });
 
-        modelBuilder.Entity<EventTag>()
+        modelBuilder
+            .Entity<EventTag>()
             .HasOne(et => et.Event)
             .WithMany(e => e.EventTags)
             .HasForeignKey(et => et.EventId);
 
-        modelBuilder.Entity<EventTag>()
+        modelBuilder
+            .Entity<EventTag>()
             .HasOne(et => et.Tag)
             .WithMany(t => t.EventTags)
             .HasForeignKey(et => et.TagId);
 
         // FavoriteEvent (many-to-many)
-        modelBuilder.Entity<FavoriteEvent>()
-            .HasKey(fe => new { fe.UserId, fe.EventId });
+        modelBuilder.Entity<FavoriteEvent>().HasKey(fe => new { fe.UserId, fe.EventId });
 
-        modelBuilder.Entity<FavoriteEvent>()
+        modelBuilder
+            .Entity<FavoriteEvent>()
             .HasOne(fe => fe.User)
             .WithMany(u => u.FavoriteEvents)
             .HasForeignKey(fe => fe.UserId);
 
-        modelBuilder.Entity<FavoriteEvent>()
+        modelBuilder
+            .Entity<FavoriteEvent>()
             .HasOne(fe => fe.Event)
             .WithMany()
             .HasForeignKey(fe => fe.EventId);
